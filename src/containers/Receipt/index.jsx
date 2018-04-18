2152c06e598bf466bd2df60a511e60e682d7c0bd
Sofien Lazreg <lazreg87@gmail.com>
2018-04-18 16:28:20 +0200

    add static receipt

--- src/containers/Receipt/index.jsx
+++ src/containers/Receipt/index.jsx
@@ -8,6 +8,31 @@ import T from 'src/utils/i18n';
 class Receipt extends Reflux.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      first_name: 'James',
+      last_name: 'Bond',
+      phone_number: '+47 97180329',
+      orderId: 'ERD232',
+      totalPrice: '200'
+    };
+  }
+
+  renderReceiptItem(label, items = []) {
+    let multipleClassName = null;
+    if (items.length > 1) multipleClassName = 'receipt-item__multiple';
+
+    const children = items.map((item) => (<span className={multipleClassName}>{item}</span>));
+
+    return (
+      <li className="receipt-item">
+        <div>
+          <span>Name:</span>
+        </div>
+        <div>
+          {children}
+        </div>
+      </li>
+    );
   }
 
   render() {
@@ -17,7 +42,63 @@ class Receipt extends Reflux.Component {
           <span className="header-title">{T.translate('receipt.header')}</span>
         </Header>
         <div className="kvass-widget__content-body">
-          <div className="content">
+          <div className="receipt">
+            <div className="padding-container">
+              <div className="receipt-summary">
+                <ul>
+                  <li className="receipt-item">
+                    <div>
+                      <span>Name:</span>
+                    </div>
+                    <div>
+                      <span>{this.state.first_name} {this.state.last_name}</span>
+                    </div>
+                  </li>
+                  <li className="receipt-item">
+                    <div>
+                      <span>Phone number:</span>
+                    </div>
+                    <div>
+                      <span>{this.state.phone_number}</span>
+                    </div>
+                  </li>
+                  <li className="receipt-item">
+                    <div>
+                      <span>Order:</span>
+                    </div>
+                    <div>
+                      <span>{this.state.orderId}</span>
+                    </div>
+                  </li>
+                  <li className="receipt-item">
+                    <div>
+                      <span>Product(s):</span>
+                    </div>
+                    <div className="receipt-item__multiple">
+                      <span>2x Example Product</span>
+                      <span>1x Example Product2</span>
+                    </div>
+                  </li>
+                  <li className="receipt-item">
+                    <div>
+                      <span>Total price:</span>
+                    </div>
+                    <div>
+                      <span>{this.state.totalPrice}</span>
+                    </div>
+                  </li>
+                  <li className="receipt-item">
+                    <div>
+                      <span>Delivery:</span>
+                    </div>
+                    <div className="receipt-item__multiple">
+                      <span>Augestadveien 14A, 1413 Tårnåsen</span>
+                      <span>20/4/2018 18:00</span>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
           <div className="kvass-widget__content-footer">
             <div className="footer-content">
