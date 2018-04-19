07e2b48fc057dac3abb813cc6bd951d8637f6af9
Sofien Lazreg <lazreg87@gmail.com>
2018-04-19 09:32:19 +0200

    get receipt data from stores

--- src/containers/Receipt/index.jsx
+++ src/containers/Receipt/index.jsx
@@ -3,29 +3,20 @@ import Reflux from 'reflux';
 import {DebounceInput} from 'react-debounce-input';
 import Header from 'src/components/Header';
 import Actions from 'src/reflux/Actions';
+import DeliveryStore from 'src/reflux/DeliveryStore';
+import ProductStore from 'src/reflux/ProductStore';
+import UserStore from 'src/reflux/UserStore';
 import T from 'src/utils/i18n';
 
 class Receipt extends Reflux.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      first_name: 'James',
-      last_name: 'Bond',
-      phone_number: '+47 97180329',
-      orderId: 'ERD232',
-      totalPrice: '200',
-      products: [
-        '2x Example Product',
-        '1x Example Product2'
-      ],
-      delivery: [
-        'Augestadveien 14A, 1413 Tårnåsen',
-        '20/4/2018 18:00'
-      ]
-    };
+    this.stores = [DeliveryStore, ProductStore, UserStore];
+    this.storeKeys = ['firstName', 'lastName', 'phoneNumber', 'products', 'totalSum', 'deliveryAddress', 'deliveryTime', 'deliveryDate', 'deliveryAdditional'];
   }
 
   renderReceiptItem(label, items = []) {
+    console.log(items);
     let multipleClassName = null;
     if (items.length > 1) multipleClassName = 'receipt-item__multiple';
 
@@ -44,6 +35,11 @@ class Receipt extends Reflux.Component {
   }
 
   render() {
+    let productArray = [];
+    Object.entries(this.state.products).forEach(([key, value]) => {
+      if (value) productArray.push(`${value.count}x ${value.item.name}`);
+    });
+
     return (
       <div className="receipt">
         <Header showBackNav={true}>
@@ -54,12 +50,13 @@ class Receipt extends Reflux.Component {
             <div className="padding-container">
               <div className="receipt-summary">
                 <ul>
-                  {this.renderReceiptItem('Name', [this.state.first_name + ' ' + this.state.last_name])}
-                  {this.renderReceiptItem('Phone number', [this.state.phone_number])}
-                  {this.renderReceiptItem('Order', [this.state.orderId])}
-                  {this.renderReceiptItem('Product(s)', this.state.products)}
-                  {this.renderReceiptItem('Total price', [this.state.totalPrice])}
-                  {this.renderReceiptItem('Delivery', this.state.delivery)}
+                  {this.renderReceiptItem('Name', [this.state.firstName + ' ' + this.state.lastName])}
+                  {this.renderReceiptItem('Phone number', [this.state.phoneNumber])}
+                  {/*this.renderReceiptItem('Order', [this.state.orderId])*/}
+                  {this.renderReceiptItem('Product(s)', productArray)}
+                  {this.renderReceiptItem('Total price', [this.state.totalSum])}
+                  {this.renderReceiptItem('Delivery', [this.state.deliveryAddress, `${this.state.deliveryDate.format('DD/MM YYYY')}, ${this.state.deliveryTime}`])}
+                  {this.state.deliveryAdditional ? this.renderReceiptItem('Delivery notes' , [this.state.deliveryAdditional]) : null}
                 </ul>
               </div>
             </div>
