1cf191f0aebf288b92935697a0581e4f62b4293b
Sofien Lazreg <lazreg87@gmail.com>
2018-04-26 13:49:33 +0200

    move footer to separate components

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Notification from 'src/components/Notification';
+
+const Footer = (props) => {
+  return (
+    <div className="kvass-widget__content-footer">
+      {props.children}
+      <Notification />
+    </div>
+  );
+};
+
+Footer.defaultProps = {
+  //showCloseNav: true
+};
+
+Footer.propTypes = {
+  //showCloseNav: PropTypes.bool
+};
+
+export default Footer;
