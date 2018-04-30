61d39af8c461474812a1fe08b0f2ca28591fd215
Sofien Lazreg <lazreg87@gmail.com>
2018-04-30 16:41:13 +0200

    remove unused fill on svg icons

--- src/components/svgIcons/addIcon.js
+++ src/components/svgIcons/addIcon.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const AddIcon = (props) => {
   const {className} = props;
   return (
-    <svg className={className} fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+    <svg className={className} height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
       <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/>
       <path d="M0 0h24v24H0z" fill="none"/>
     </svg>
