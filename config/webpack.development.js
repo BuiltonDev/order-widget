30205363f4e013fc2a99b4c84c87142311546393
Sofien Lazreg <lazreg87@gmail.com>
2018-05-02 13:19:48 +0200

    add example config and rename test to example

--- config/webpack.development.js
+++ config/webpack.development.js
@@ -5,9 +5,6 @@ const common = require('./webpack.common.js');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
-// Local development config
-const config = require('../test/config');
-
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
@@ -17,16 +14,9 @@ module.exports = merge(common, {
   },
   plugins: [
     new CleanWebpackPlugin(['../dist']),
-    new webpack.DefinePlugin({
-      APIKEY: JSON.stringify(config.apiKey),
-      ENDPOINT: JSON.stringify(config.endpoint),
-      FIREBASE_APIKEY: JSON.stringify(config.firebaseConfig.apiKey),
-      FIREBASE_DOMAIN: JSON.stringify(config.firebaseConfig.domain),
-      STRIPE_API_KEY: JSON.stringify(config.stripeApiKey)
-    }),
     new HtmlWebpackPlugin({
       title: 'Kvass Ordering Widget',
-      template: path.resolve(__dirname, '../test/index.html')
+      template: path.resolve(__dirname, '../example/index.html')
     }),
     new webpack.NamedModulesPlugin(),
     new webpack.HotModuleReplacementPlugin()
