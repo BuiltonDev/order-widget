885aa4aa8569bc9e4927dd7fa2e7404ff665f94a
Sofien Rakke Lazreg <lazreg87@gmail.com>
2018-06-19 09:39:16 +0200

    add powered by link and move it into widget itself (#61)

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import Notification from 'src/components/Notification';
+import T from 'src/utils/i18n';
+import KvassIcon from 'src/components/SvgIcons/KvassIcon';
 
 const Footer = (props) => {
   return (
     <div className="kvass-widget__content-footer">
       <div className="footer-content">
         {props.children}
+        <div className="kvass-widget_powered-by">
+          <a href="https://kvass.ai">
+            <span>{T.translate('global.poweredBy')}</span>{' '}
+            <KvassIcon className="svg-icon--powered-by"></KvassIcon>
+          </a>
+        </div>
       </div>
       <Notification />
     </div>
