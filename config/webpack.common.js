c9e49065c478b8fd761aeb1798877af9887572ca
Sofien Lazreg <lazreg87@gmail.com>
2018-06-22 16:17:55 +0200

    cleanup

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -20,7 +20,7 @@ module.exports = {
      },
      {
        test: /\.(css|scss)/,
-       use: ['style-loader', 'css-loader', 'sass-loader']
+       use: ['style-loader', 'css-loader', 'resolve-url-loader', 'sass-loader']
      }
    ]
   },
