3260eb5a94af4a464e14681c7392bb9e1f769db1
Tigran <tigrangit@gmail.com>
2018-07-24 13:11:17 +0300

    develiry details

--- src/containers/DeliveryDetails/index.jsx
+++ src/containers/DeliveryDetails/index.jsx
@@ -11,12 +11,23 @@ import T from 'src/utils/i18n';
 import Animate from 'src/utils/animate';
 
 class DeliveryDetails extends Reflux.Component {
+  static renderHeader() {
+    return (
+      <Header showBackNav={true}>
+        <span className="header-title">{T.translate('deliveryDetails.header')}</span>
+      </Header>
+    );
+  }
+
   constructor(props) {
     super(props);
 
     this.store = DeliveryStore;
     this.storeKeys = ['deliveryAdditional', 'setDate', 'setTime'];
     this.animation = new Animate();
+
+    this.renderFooter = this.renderFooter.bind(this);
+    this.renderBody = this.renderBody.bind(this);
   }
 
   componentDidMount() {
@@ -27,37 +38,67 @@ class DeliveryDetails extends Reflux.Component {
     this.animation.animateInViewTransition();
   }
 
+  renderFooter() {
+    const {setDate, setTime} = this.state;
+    return (
+      <Footer>
+        <button
+          className="kvass-widget__primary-button"
+          disabled={!setDate || !setTime}
+          onClick={() => Actions.onNextNavigation()}
+        >
+          {T.translate('global.next')}
+        </button>
+      </Footer>
+    );
+  }
+
+  renderBody() {
+    const {deliveryAdditional} = this.state;
+    return (
+      <div className="padding-container">
+        <div className='in-page-transition'>
+          <p>
+            {T.translate('deliveryDetails.deliveryDetails')}
+          </p>
+          <PlaceAutoCompleteWrapper />
+        </div>
+        <div className='in-page-transition'>
+          <p>
+            {T.translate('deliveryDetails.dateDetails')}
+          </p>
+          <DayPickerWrapper />
+        </div>
+        <div className='in-page-transition'>
+          <p>
+            {T.translate('deliveryDetails.timeDetails')}
+          </p>
+          <TimePickerWrapper />
+        </div>
+        <div className='in-page-transition'>
+          <p>{T.translate('deliveryDetails.additionalDetails')}</p>
+          <textarea
+            className="delivery-details__additional"
+            value={deliveryAdditional}
+            onChange={
+              evt => Actions.onAdditionalDetailsChange(evt.target.value)
+            }
+            maxLength="250"
+          />
+        </div>
+      </div>
+    );
+  }
+
   render() {
-    const {deliveryAdditional, setDate, setTime} = this.state;
     return (
       <div className="delivery-details">
-        <Header showBackNav={true}>
-          <span className="header-title">{T.translate('deliveryDetails.header')}</span>
-        </Header>
+        {this.constructor.renderHeader()}
         <div className="kvass-widget__content-body">
           <div className="content">
-            <div className="padding-container">
-              <div className='in-page-transition'>
-                <p>{T.translate('deliveryDetails.deliveryDetails')}</p>
-                <PlaceAutoCompleteWrapper />
-              </div>
-              <div className='in-page-transition'>
-                <p>{T.translate('deliveryDetails.dateDetails')}</p>
-                <DayPickerWrapper />
-              </div>
-              <div className='in-page-transition'>
-                <p>{T.translate('deliveryDetails.timeDetails')}</p>
-                <TimePickerWrapper />
-              </div>
-              <div className='in-page-transition'>
-                <p>{T.translate('deliveryDetails.additionalDetails')}</p>
-                <textarea className="delivery-details__additional" value={deliveryAdditional} onChange={(evt) => Actions.onAdditionalDetailsChange(evt.target.value)} maxLength="250"/>
-              </div>
-            </div>
+            {this.renderBody()}
           </div>
-          <Footer>
-            <button className="kvass-widget__primary-button" disabled={!setDate || !setTime} onClick={() => Actions.onNextNavigation()}>{T.translate('global.next')}</button>
-          </Footer>
+          {this.renderFooter()}
         </div>
       </div>
     );
