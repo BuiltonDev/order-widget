d34edc88d13498a31d3c35b518856fe7e16e0da8
Tigran <tigrangit@gmail.com>
2018-07-24 11:25:28 +0300

    footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,23 +1,33 @@
 import React from 'react';
 import Notification from 'src/components/Notification';
 import T from 'src/utils/i18n';
+import PropTypes from 'prop-types';
 import KvassIcon from 'src/components/SvgIcons/KvassIcon';
 
-const Footer = (props) => {
-  return (
-    <div className="kvass-widget__content-footer">
-      <div className="footer-content">
-        {props.children}
-        <div className="kvass-widget_powered-by">
-          <a href="https://kvass.ai">
-            <span>{T.translate('global.poweredBy')}</span>{' '}
-            <KvassIcon className="svg-icon--powered-by"></KvassIcon>
-          </a>
-        </div>
+const Footer = props => (
+  <div className="kvass-widget__content-footer">
+    <div className="footer-content">
+      {props.children}
+      <div className="kvass-widget_powered-by">
+        <a href="https://kvass.ai">
+          <span>{T.translate('global.poweredBy')}</span>{' '}
+          <KvassIcon className="svg-icon--powered-by" />
+        </a>
       </div>
-      <Notification />
     </div>
-  );
+    <Notification />
+  </div>
+);
+
+Footer.defaultProps = {
+  children: undefined
+};
+
+Footer.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.array,
+    PropTypes.instanceOf(React.Component)
+  ])
 };
 
 export default Footer;
