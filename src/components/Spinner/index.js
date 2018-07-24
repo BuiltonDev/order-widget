3fa161404609b2f9989ec0821c875d0e45ac01c6
Tigran <tigrangit@gmail.com>
2018-07-24 12:32:15 +0300

    spinner

--- src/components/Spinner/index.js
+++ src/components/Spinner/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'classnames';
 
 const Spinner = (props) => {
   const {show, showOverlay} = props;
-  const spinnerClass = classNames({
-    'spinner-container': true,
-    'spinner-container--hidden': !show,
-    overlay: showOverlay
-  });
   return (
-    <div className={spinnerClass}>
-      <div className="loader"></div>
+    <div className={`spinner-container ${showOverlay && 'overlay'} ${!show && 'spinner-container--hidden'}`}>
+      <div className="loader" />
     </div>
   );
 };
